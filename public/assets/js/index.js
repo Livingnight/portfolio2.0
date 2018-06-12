6f935ded18c598a417f857a4c850b170e64af6d9
unknown <jjernigan1065@gmail.com>
2018-06-12 12:46:41 -0400

    disappearing/reappearing nav bar working

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    $(window).scroll(
+        {
+            previousTop: 0
+        },
+        function () {
+            var currentTop = $(window).scrollTop();
+            if (currentTop <= this.previousTop) {
+                $(".container em").text("");
+                $("nav").css("opacity", "1");
+            } else {
+                $(".container em").text("");
+                $("nav").css("opacity", "0");
+            }
+            this.previousTop = currentTop;
+        });
 });
