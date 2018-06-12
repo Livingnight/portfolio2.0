fa0497559c92f3db043863c6a1d0de2190c1eb64
unknown <jjernigan1065@gmail.com>
2018-06-12 09:51:55 -0400

    backend complete. working on front end

--- controller/portfolioController.js
+++ controller/portfolioController.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    // res.send('hello world');
     res.render('index');
 });
 
@@ -13,3 +14,5 @@ router.get('contact', (req, res) => {
 router.get('portfolio', (req, res) => {
     res.render('portfolio');
 });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const exphbs = requre('express-handlebars');
+const exphbs = require('express-handlebars');
 const path = require('path');
+const routes = require('./controller/portfolioController.js');
+
 
 const PORT = process.env.PORT || 17000;
 
@@ -9,7 +11,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParser.urlEncoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json());
 
@@ -17,4 +19,7 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 
 app.set('view engine', 'handlebars');
 
-const routes = require('controller/portfolioController');
+app.use(routes)
+app.listen(PORT, () =>{
+    console.log(`server listening on PORT: ${PORT}`);
+});
